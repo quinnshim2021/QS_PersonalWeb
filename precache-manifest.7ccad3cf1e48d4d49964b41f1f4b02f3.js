38612d6d8abffdeb0a115ec5f1acc7fb74240b12
quinnrs98 <quinnrs98@gmail.com>
2019-11-25 17:46:06 -0600

    Updates

---
+++ precache-manifest.7ccad3cf1e48d4d49964b41f1f4b02f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "601408bbad62f2009e8e2cf50eb65f09",
+    "url": "//quinnshim2021.github.io/QS_PersonalWeb/index.html"
+  },
+  {
+    "revision": "ba44ecc99be2b47b22ae",
+    "url": "//quinnshim2021.github.io/QS_PersonalWeb/static/css/main.3e6aa532.chunk.css"
+  },
+  {
+    "revision": "2942467dec90903116b1",
+    "url": "//quinnshim2021.github.io/QS_PersonalWeb/static/js/2.b35355c8.chunk.js"
+  },
+  {
+    "revision": "ba44ecc99be2b47b22ae",
+    "url": "//quinnshim2021.github.io/QS_PersonalWeb/static/js/main.193b0938.chunk.js"
+  },
+  {
+    "revision": "8f508431e9d786f09809",
+    "url": "//quinnshim2021.github.io/QS_PersonalWeb/static/js/runtime-main.b31ff248.js"
+  },
+  {
+    "revision": "f7b7a17681eb4928e22cb049602d69b0",
+    "url": "//quinnshim2021.github.io/QS_PersonalWeb/static/media/profile.f7b7a176.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "//quinnshim2021.github.io/QS_PersonalWeb/precache-manifest.7ccad3cf1e48d4d49964b41f1f4b02f3.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("//quinnshim2021.github.io/QS_PersonalWeb/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
